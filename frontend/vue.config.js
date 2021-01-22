96ab46b381606e314921306844cb42e3b11f2473
getzch <keepprogress@gmail.com>
2021-01-22 09:03:46 +0800

    use devServer.proxy to help us with SOP

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,17 @@
   module.exports = {
     // options...
     outputDir: 'target/dist',
-    assetsDir: 'static'
+    assetsDir: 'static',
+  // proxy all webpack dev-server requests starting with /api
+  // to our Spring Boot backend (localhost:8098) using http-proxy-middleware
+  // see https://cli.vuejs.org/config/#devserver-proxy
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8098',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
   }

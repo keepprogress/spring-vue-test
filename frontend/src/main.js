3720553479b9c6b6e9b7c075e02ac789c79f7ee3
getzch <keepprogress@gmail.com>
2021-01-23 16:01:42 +0800

    add dev-tool dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios';
-import 'bootstrap';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import VueAxios from 'vue-axios';
 
-Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Bootstrap from '@/component/Bootstrap'
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,11 @@ const router = new VueRouter({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+      path: '/bootstrap',
+      name: 'Bootstrap',
+      component: Bootstrap
     }
   ],
   mode: 'history',

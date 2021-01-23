98660cedcd89481d3ad10c88a4b93f20c84cf865
getzch <keepprogress@gmail.com>
2021-01-23 17:34:36 +0800

    fix error for run build

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue';
+import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import VueAxios from 'vue-axios';
-
 Vue.config.productionTip = false
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,31 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Bootstrap from '@/component/Bootstrap'
+import Bootstrap from '@/components/Bootstrap'
 
 Vue.use(VueRouter)
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/bootstrap',
+    name: 'Bootstrap',
+    component: Bootstrap    
+  }
+]
+
 const router = new VueRouter({
-  routes = [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'About',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    },
-    {
-      path: '/bootstrap',
-      name: 'Bootstrap',
-      component: Bootstrap
-    }
-  ],
   mode: 'history',
   base: process.env.BASE_URL,
   routes
